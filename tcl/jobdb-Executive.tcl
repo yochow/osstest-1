49b74c97766d2c15166e25cf800a4d8a7a6ea3fd
Ian Jackson <iwj@woking.cam.xci-test.com>
2012-10-12 19:20:30 +0100

    reorg wip

--- tcl/jobdb-Executive.tcl
+++ tcl/jobdb-Executive.tcl
@@ -3,34 +3,6 @@
 package require Tclx
 package require Pgtcl 1.5
 
-proc readconfig {} {
-    global c
-    set pl {
-        use Osstest;
-        csreadconfig();
-        foreach my $k (sort keys %c) {
-            my $v= $c{$k};
-            printf "%s\n%d\n%s\n", $k, length($v), $v;
-        }
-    }
-    set ch [open |[list perl -e $pl] r]
-    while {[gets $ch k] >= 0} {
-        gets $ch vl
-        set v [read $ch $vl]
-        if {[eof $ch]} { error "premature eof in $k" }
-        set c($k) $v
-        gets $ch blank
-        if {[string length $blank]} { error "$blank ?" }
-    }
-    close $ch
-}
-
-proc log {m} {
-    set now [clock seconds]
-    set timestamp [clock format $now -format {%Y-%m-%d %H:%M:%S Z} -gmt 1]
-    puts "$timestamp $m"
-}
-
 proc logputs {f m} {
     global argv
     set time [clock format [clock seconds] -gmt true \
@@ -309,25 +281,6 @@ proc step-set-status {flight job stepno st} {
     }
 }
 
-proc reap-ts {reap {dbopen 0}} {
-    if {![string length $reap]} { if {$dbopen} db-open; return 0 }
-
-    upvar #0 reap_details($reap) details
-    set detstr [lindex $details 3]
-    set iffail [lindex $details 4]
-    logputs stdout "awaiting $detstr"
-    if {[catch { close $reap } emsg]} {
-        set result $iffail
-    } else {
-        set result pass
-    }
-    if {$dbopen} db-open
-
-    eval step-set-status [lrange $details 0 2] $result
-    logputs stdout "finished $detstr $result $emsg"
-    return [expr {![string compare $result pass]}]
-}
-
 proc transaction {tables script} {
     db-open
     while 1 {
@@ -383,18 +336,4 @@ proc become-task {comment} {
     if {$nrows != 1} {
         error "$nrows $taskid $refinfo ?"
     }
-}
-
-proc must-gets {chan regexp args} {
-    if {[gets $chan l] <= 0} { error "[eof $chan] $regexp" }
-    if {![uplevel 1 [list regexp $regexp $l dummy] $args]} {
-        error "$regexp $l ?"
-    }
-}
-
-proc lremove {listvar item} {
-    upvar 1 $listvar list
-    set ix [lsearch -exact $list $item]
-    if {$ix<0} return
-    set list [lreplace $list $ix $ix]
 }
