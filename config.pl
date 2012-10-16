cae1a3f50209cc1d29dab200f57236a9718743d6
Ian Jackson <iwj@woking.cam.xci-test.com>
2012-10-16 18:23:40 +0100

    wip reorg continuing with config etc.

--- config.pl
+++ config.pl
@@ -5,8 +5,6 @@ $c{SerialLogPattern}= '/root/sympathy/%host%.log*';
 
 $c{Tftp}= '/tftpboot/pxe';
 
-$c{TftpPxeGroup}= 'osstest';
-
 $c{PxeDiBase}= 'osstest/debian-installer';
 
 $c{PxeDiVersion}= '2012-01-30-squeeze';
