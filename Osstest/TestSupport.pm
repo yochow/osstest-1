yochow/osstest-1

commit 20387acf7798e9019cdf6df4a875b8ffc079a979

TestSupport.pm: introduce iso_create_xorriso
... and convert ts-redhat-install to make use of it.

Signed-off-by: Wei Liu <wei.liu2@citrix.com>
Acked-by: Ian Jackson <ian.jackson@eu.citrix.com>

Osstest/TestSupport.pm
@@ -100,6 +100,8 @@ BEGIN {
                       setup_pxeboot setup_pxeboot_local host_pxefile
 
                       ether_prefix
+
+                      iso_create_xorriso
                       );
     %EXPORT_TAGS = ( );
 
@@ -1885,6 +1887,20 @@ timeout 5
 label local
 	LOCALBOOT 0
 default local
+END
+}
+
+#---------- ISO images ----------
+sub iso_create_xorriso ($$$$;@) {
+    my ($ho,$iso,$dir,$isotimeout,@xopts) = @_;
+
+    # These options forces ISO creation even if input directory is
+    # empty. They have to go last.
+    my @force_iso_creation = qw(-- -changes_pending yes);
+
+    target_cmd_root($ho, <<END, 60);
+        mkdir -p $dir
+        xorriso @xopts -o $iso $dir/. @force_iso_creation
 END
 }
 

ts-redhat-install
@@ -112,7 +112,7 @@ END
 our $emptyiso= '/root/empty.iso';
 
 sub prep () {
-    target_install_packages_norec($ho, qw(lvm2 rsync genisoimage));
+    target_install_packages_norec($ho, qw(lvm2 rsync xorriso));
 
     my $isotimeout= 600;
 
@@ -123,18 +123,15 @@ sub prep () {
     my $newiso= '/root/newiso';
     my $emptydir= '/root/empty-dir';
 
-    my @isogen_base= qw(-R -J -T);
+    my @isogen_base= qw(-as mkisofs -R -J);
     my @isogen_boot= qw(-b isolinux/isolinux.bin
                         -c isolinux/boot.cat
                         -no-emul-boot
                         -boot-load-size 4
                         -boot-info-table);
+    my @isogen= (@isogen_base, @isogen_boot);
 
-    target_cmd_root($ho, <<END, 60);
-            set -e
-            mkdir -p $emptydir
-            genisoimage -o $emptyiso @isogen_base $emptydir/.
-END
+    iso_create_xorriso($ho, $emptyiso, $emptydir, 60, @isogen_base);
 
     more_prepareguest_hvm($ho,$gho, $ram_mb, $disk_mb,
                           OnReboot => 'preserve',
@@ -163,9 +160,7 @@ END
         target_putfilecontents_root_stash($ho, 10, kickstart(),
                                           "$newiso/ks.cfg");
 
-        target_cmd_root($ho, <<END, $isotimeout);
-            genisoimage -o $gho->{Rimage} @isogen_base @isogen_boot $newiso/.
-END
+        iso_create_xorriso($ho, $gho->{Rimage}, $newiso, $isotimeout, @isogen);
     });
 }
 

commit 885926f6b7aa438d991238ba38c980fcf3048687

iso_create_genisoimage: Honour $isotimeout
Fixes regression introduced in 20387acf, where the newly-moved code
used a hardwired value of 60 rather than the parameter provided for
the purpose.

Signed-off-by: Ian Jackson <Ian.Jackson@eu.citrix.com>

Osstest/TestSupport.pm
@@ -1904,7 +1904,7 @@ END
 sub iso_create_genisoimage ($$$$;@) {
     my ($ho,$iso,$dir,$isotimeout,@xopts) = @_;
 
-    target_cmd_root($ho, <<END, 60);
+    target_cmd_root($ho, <<END, $isotimeout);
         mkdir -p $dir
         genisoimage @xopts -o $iso $dir/.
 END